904cbb307f770146297b2d7c5abcf4846edbb282
sys <kostya.sys@gmail.com>
2012-10-12 17:44:10 +0400

    fuck yeah

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+$: << File.dirname(__FILE__)
+
 require "bundler"
 Bundler.setup
 
@@ -6,34 +8,97 @@ require 'goliath/rack/templates'
 require 'haml'
 require "tilt"
 
-
-class CallbacksRegistry
-  def self.get_downloader
-    @@env
-  end
-
-  def self.register_downloader(down_stream)
-    @@env = down_stream
-  end
-end
+require "parser"
 
 class Stream < Goliath::API
+
+  def initialize
+    @parsers = {}
+  end
+
+  def downloader_connected?
+    !@downloader_env.nil?
+  end
+
+  def write_download_stream(data)
+    @downloader_env.stream_send(data) if @downloader_env
+  end
+
+  def add_download_stream(down_stream)
+    @downloader_env = down_stream
+  end
+
+  def close_download_stream
+    return unless @downloader_env
+    env.logger.info 'closing download stream'
+    @downloader_env.stream_send 'closing connection'
+    @downloader_env[:closed] = true
+    @downloader_env.stream_close
+    @downloader_env = nil
+    'closed'
+  end
+
   include Goliath::Rack::Templates
 
   def on_headers(env, headers)
-    env.logger.info 'received headers: ' + headers.inspect
+    if env['REQUEST_METHOD'] == 'POST'
+      env[:uploading] = true
+      env.logger.info 'Upload size: ' + headers['Content-Length'].to_s
+
+      #raise Goliath::Validation::NotImplementedError.new("no downloader yet") unless downloader_connected?
+    end
+
+    if env['PATH_INFO'] == '/download'
+      env[:download_stream] = true
+    end
+
     env['async-headers'] = headers
   end
 
   def on_body(env, data)
-    env.logger.info 'received data: ' + data
-    CallbacksRegistry.get_downloader.stream_send data
+    #env[:request_id] ||= get_request_id
+    #_id = env[:request_id]
+    unless env[:boundary_end]
+      unless env['CONTENT_TYPE'] =~ MULTIPART
+        raise Goliath::Validation::VerificationError.new('no file given')
+      end
+      boundary = $1
+      boundary_start = "--#{boundary}"
+      boundary_end = /(?:#{EOL})?#{Regexp.quote(boundary)}(#{EOL}|--)/n
+
+      head_end = data.index(EOL+EOL)
+      head = data.slice(0, head_end + 4)
+      env.logger.info head
+
+      env[:boundary_end] = boundary_end
+
+      data.slice!(0, head_end+4)
+    end
+
+    if data =~ env[:boundary_end]
+      boundary_end = data.index($1)
+      data.slice!(boundary_end, data.size)
+    end
+
+    #unless @parsers[_id]
+    #
+    #  parser = Parser.new
+    #  @parsers[_id] = parser
+    #  env.logger.info 'uploading'
+    #  env.logger.info 'block size: ' + data.length.to_s
+    #  parser.on :part_data do |buf, from, to|
+    write_download_stream(data)
+      #end
+    #end
+    #
+
   end
 
   def on_close(env)
-    CallbacksRegistry.get_downloader.stream_close
-    env.logger.info 'closing connection'
+    env.logger.info env[:uploading]
+    close_download_stream if env[:uploading]
   end
+
 
   def response(env)
     case env['PATH_INFO']
@@ -41,8 +106,17 @@ class Stream < Goliath::API
         then [200, {}, haml(:index)]
       when '/download'
         then
-            CallbacksRegistry.register_downloader(env)
+            add_download_stream(env)
             [200, {}, Goliath::Response::STREAMING]
+      else
+        [200, {'Content-type' => 'text/plain'}, env['async-headers']]
     end
+  end
+
+  def get_request_id
+    @id ||= 0
+    @id+=1
+
+    return @id
   end
 end
